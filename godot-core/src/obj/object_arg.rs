7d3da4c9d62c21b5702aed3394d1096772b5715b
egsam98 <egsam98@gmail.com>
2024-08-03 19:30:33 +0300

    unfix AsObjectArg

--- godot-core/src/obj/object_arg.rs
+++ godot-core/src/obj/object_arg.rs
@@ -33,6 +33,16 @@ where
     fn as_object_arg(&self) -> ObjectArg<T>;
 }
 
+impl<T, U> AsObjectArg<T> for Gd<U>
+where
+    T: GodotClass + Bounds<Declarer = bounds::DeclEngine>,
+    U: Inherits<T>,
+{
+    fn as_object_arg(&self) -> ObjectArg<T> {
+        unsafe { ObjectArg::from_raw_gd(&self.raw) }
+    }
+}
+
 impl<T, U> AsObjectArg<T> for &Gd<U>
 where
     T: GodotClass + Bounds<Declarer = bounds::DeclEngine>,
